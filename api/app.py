050eb4edecf61da2784ff6ea98abccc40e3a0529
Sam <90465673+TheoIsen@users.noreply.github.com>
2024-12-11 12:36:05 +0100

    feat: Initialisation des routes de l'api

--- api/app.py
+++ api/app.py
@@ -6,5 +6,18 @@ app = Flask(__name__)
 def home():
     return jsonify({"message": "Welcome to the Shop API!"})
 
+# Routes for the Shop API : items, baskets, users
+@app.route("/items")
+def items():
+    return jsonify({"message": "List of all items"})
+
+@app.route("/baskets")
+def baskets():
+    return jsonify({"message": "List of all baskets"})
+
+@app.route("/users")
+def users():
+    return jsonify({"message": "List of all users"})
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
